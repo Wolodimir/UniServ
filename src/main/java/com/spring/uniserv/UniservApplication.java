bb5700949e7727358a2aca94837fa36a66f939ca
Wolodimir <dvdent666@gmail.com>
2021-01-23 15:55:13 +0400

    maven init

---
+++ src/main/java/com/spring/uniserv/UniservApplication.java
@@ -0,0 +1,13 @@
+package com.spring.uniserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniservApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniservApplication.class, args);
+    }
+
+}
